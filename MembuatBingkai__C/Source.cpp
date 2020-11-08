2bb9d9eac9b9f35540601d6cebdc30860dd9e46e
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-11-08 18:12:49 +0700

    Add project files.

---
+++ MembuatBingkai__C/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	cout << "\tษออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออป\n";
+	cout << "\tบ                        RIZKY KHAPIDSYAH                          บ\n";
+	cout << "\tบ                           Version 1.0                            บ\n";
+	cout << "\tบ                     Command Line Interpreter                     บ\n";
+	cout << "\tศออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออออผ\n";
+
+	_getch();
+	return 0;
+}
